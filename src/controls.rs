8e396c55e7c7cbf72c91e2950b757018779476a7
Luna <ciubix8514@gmail.com>
2023-04-21 19:04:40 +0300

    Added begginings of the ui

--- src/controls.rs
+++ src/controls.rs
@@ -1,18 +1,46 @@
-// use iced::Command;
-use iced_wgpu;
 use iced_winit::{
-    widget::{ text, Row, row, container},
-    Command, Program, Length,
+    alignment, column, row,
+    widget::{button, container, pick_list},
+    Command, Length, Program,
 };
 
-pub struct Controls {}
+#[derive(Default, Clone, Debug,PartialEq,Eq)]
+pub enum Fractals {
+    #[default]
+    Mandelbrot = 1,
+    BurningShip = 2,
+    Tricorn = 4,
+    Feather = 8,
+    Eye = 16,
+}
+
+impl std::fmt::Display for Fractals {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Fractals::Mandelbrot => write!(f,"Mandelbrot set"),
+            Fractals::BurningShip => write!(f,"Burning ship"),
+            Fractals::Tricorn => write!(f,"Tricorn"),
+            Fractals::Feather => write!(f,"Feather"),
+            Fractals::Eye => write!(f,"Eye"),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Controls {
+    ui_open: bool,
+    current_fractal: Fractals,
+}
 
 #[derive(Debug, Clone)]
-pub enum Message {}
+pub enum Message {
+    ToggleUi,
+    ChangeFractal(Fractals),
+}
 
 impl Controls {
     pub fn new() -> Controls {
-        Controls {}
+        Controls::default()
     }
 }
 
@@ -22,12 +50,27 @@ impl Program for Controls {
     type Message = Message;
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::ToggleUi => self.ui_open = !self.ui_open,
+            Message::ChangeFractal(f) => self.current_fractal = f,
+        }
         Command::none()
     }
 
     fn view(&self) -> iced_winit::Element<'_, Self::Message, Self::Renderer> {
-        let t = text("This is a test").into();
-        let content=  row(vec![t]);
-        container(content).width(Length::Fill).center_x().into()
+        let content = if !self.ui_open {
+            let open_button = button("Open UI").on_press(Message::ToggleUi);
+            row![open_button].padding(10)
+        } else {
+            let close_button = button("Close").on_press(Message::ToggleUi);
+            let fractals = vec![Fractals::Mandelbrot,Fractals::BurningShip,Fractals::Tricorn,Fractals::Feather,Fractals::Eye];
+            let fractal_list =
+                pick_list(fractals,Some(self.current_fractal.clone()) , Message::ChangeFractal);
+            row![column![close_button,fractal_list]]
+        };
+        container(content)
+            .width(Length::Fill)
+            .align_x(alignment::Horizontal::Right)
+            .into()
     }
 }
